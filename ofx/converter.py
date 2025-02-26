da2e7bbea5ae76c0cea7aecf34de496c9cf696b0
wsleeal <wsleal@yahoo.com>
2025-02-25 22:27:42 -0300

    update converter.py

--- ofx/converter.py
+++ ofx/converter.py
@@ -1,9 +1,11 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
 import ofxparse
 import pandas as pd
-import argparse
+import os
 
 
-def convert_ofx_to_csv_excel(ofx_file):
+def convert_ofx_to_excel(ofx_file, output_file):
     with open(ofx_file, "rb") as f:
         ofx = ofxparse.OfxParser.parse(f)
 
@@ -16,7 +18,6 @@ def convert_ofx_to_csv_excel(ofx_file):
                 "DATA": account.date.strftime("%Y-%m-%d"),
                 "MEMORANDO": account.memo,
                 "VALOR": account.amount,
-                "BENEFICIÁRIO": account.payee,
                 "TIPO": account.type,
             }
         )
@@ -24,16 +25,36 @@ def convert_ofx_to_csv_excel(ofx_file):
     df = pd.DataFrame(transactions)
     df["ID"] = pd.to_numeric(df["ID"], errors="coerce")
     df["VALOR"] = pd.to_numeric(df["VALOR"], errors="coerce")
-    output_file = ofx_file.rsplit(".", 1)[0] + ".xlsx"
 
     df.to_excel(output_file, index=False, engine="openpyxl")
-    print(f"Arquivo convertido e salvo como: {output_file}")
+    messagebox.showinfo("Sucesso", f"Arquivo convertido e salvo como: {output_file}")
+
+
+def selecionar_arquivo():
+    input_file = filedialog.askopenfilename(filetypes=[("Arquivos OFX", "*.ofx")])
+    if input_file:
+        output_file = filedialog.asksaveasfilename(
+            defaultextension=".xlsx", filetypes=[("Planilhas Excel", "*.xlsx")]
+        )
+        if output_file:
+            convert_ofx_to_excel(input_file, output_file)
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Converter arquivo OFX para CSV ou Excel"
+    root = tk.Tk()
+    root.title("Conversor OFX para Excel")
+
+    frame = tk.Frame(root, padx=20, pady=20)
+    frame.pack(padx=10, pady=10)
+
+    label = tk.Label(
+        frame, text="Selecione um arquivo OFX para conversão e escolha onde salvar:"
     )
-    parser.add_argument("ofx_file", help="Caminho do arquivo OFX")
-    args = parser.parse_args()
-    convert_ofx_to_csv_excel(args.ofx_file)
+    label.pack()
+
+    botao_selecionar = tk.Button(
+        frame, text="Selecionar Arquivo", command=selecionar_arquivo
+    )
+    botao_selecionar.pack(pady=10)
+
+    root.mainloop()
